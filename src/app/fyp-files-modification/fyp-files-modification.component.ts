36577f1655360c50589f1eadda13283e83c62d12
nizar04 <33844522+nizar04@users.noreply.github.com>
2019-12-01 13:19:09 +0100

    majormodification

--- src/app/fyp-files-modification/fyp-files-modification.component.ts
+++ src/app/fyp-files-modification/fyp-files-modification.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TeacherServiceService } from '../services/Teacher/teacher-service.service';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/security/authentication.service';
+import { FypFileModification } from '../models/fyp/fyp-modification';
 
 @Component({
   selector: 'app-fyp-files-modification',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fyp-files-modification.component.css']
 })
 export class FypFilesModificationComponent implements OnInit {
-
-  constructor() { }
+  FypFileModification:FypFileModification[];
+  restApi: TeacherServiceService;
+  router: Router;
+  constructor(
+    private auth:AuthenticationService,
+     restApi: TeacherServiceService, 
+     router: Router
+  ) {this.restApi=restApi;
+  this.router=router;}
 
   ngOnInit() {
+    this.restApi.GetFYPFIemodifications().subscribe(files => {
+      this.FypFileModification = files as FypFileModification[]});
+      console.log(this.FypFileModification);
+
   }
 
 }
