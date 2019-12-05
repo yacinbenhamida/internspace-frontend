ebdc77a8ed1f7fd03f0df9af43338d022e62f817
Ayadi Tahar <ayaditahero2016@gmail.com>
2019-12-05 17:52:48 +0100

    University

--- src/app/models/university/university.ts
+++ src/app/models/university/university.ts
@@ -1,3 +1,13 @@
-export class University{
-    id: number
-}
+import { Site } from "./Site";
+
+export interface University {
+    id: number;
+    name: string;
+    openingYear: Date;
+    owner: string;
+    logoUrl: string;
+    location: string;
+    fypClassYear: number;
+    sites: Site[];
+  }
+  
