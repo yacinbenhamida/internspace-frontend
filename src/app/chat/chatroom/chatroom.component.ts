51edd322f23803d3a53475b47b6ae0bd8a93d00b
Yassine Ben Hamida <yacinbenhamida@hotmail.fr>
2019-12-10 21:34:57 +0100

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/app/chat/chatroom/chatroom.component.ts
+++ src/app/chat/chatroom/chatroom.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ChatService } from 'src/app/services/exchanges/chat.service';
-import { ActivatedRoute } from '@angular/router';
 import { GoogleAuthService } from 'src/app/services/security/google-auth.service';
 import { AuthenticationService } from 'src/app/services/security/authentication.service';
 
